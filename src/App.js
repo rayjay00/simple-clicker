b32486b5e0b13d1c7930165ab94d1c7e6cc3ff77
Ryan <hello@ryanjordal.me>
2019-03-03 12:27:18 -0500

    simple clicker finished

--- src/App.js
+++ src/App.js
@@ -3,23 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    count: 0
+  }
+
+  handleClick() {
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
+
+  handleReset() {
+    this.setState({
+      count: 0
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h2>Simple Clicker</h2>
+        <button onClick={this.handleClick.bind(this)}>Click Me!</button>
+        <p>Times Clicked: {this.state.count}</p>
+        <button onClick={this.handleReset.bind(this)}>Reset</button>
       </div>
     );
   }
